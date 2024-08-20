ef15381adfa5b378e7c6246740cbdec5fdcc9157
Raphael Barboza <eletronic.raphael@gmail.com>
2024-08-20 05:54:59 -0400

    Base Website

---
+++ resources/views/website.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>15333 - Vereador Zancanaro - Protegendo Vidas | Transporte, Infraestrutura e Mobilidade Urbana</title>
+    <meta name="description" content="Página oficial do vereador Zancanaro, focada em promover melhorias na infraestrutura, transporte e mobilidade urbana. Conheça as conquistas e projetos realizados durante o mandato.">
+    <meta name="keywords" content="Vereador Zancanaro, Mobilidade Urbana, Infraestrutura, Transporte, Políticas Públicas, Obras, CMTU, Urbanismo, Projetos de Lei">
+    <meta name="author" content="Vereador Zancanaro">
+    <meta name="robots" content="index, follow">
+    <meta property="og:title" content="Vereador Zancanaro - Protegendo Vidas | Transporte, Infraestrutura e Mobilidade Urbana">
+    <meta property="og:description" content="Conheça as iniciativas e conquistas do vereador Zancanaro em sua atuação na melhoria da infraestrutura, transporte e mobilidade urbana.">
+    <meta property="og:image" content="{{asset('images/line-02.png')}}">
+    <meta property="og:url" content="{{route('website')}}">
+    <meta property="og:type" content="website">
+    <meta name="twitter:card" content="{{asset('images/line-02.png')}}">
+    <meta name="twitter:title" content="Vereador Zancanaro - Protegendo Vidas | Transporte, Infraestrutura e Mobilidade Urbana">
+    <meta name="twitter:description" content="Acompanhe as ações e conquistas do vereador Zancanaro em prol de uma cidade mais segura e desenvolvida.">
+    <meta name="twitter:image" content="{{asset('images/line-02.png')}}">
+    <link rel="canonical" href="{{route('website')}}">
+    <link rel="icon" type="image/png" href="{{asset('images/mdb-icone.png')}}" />
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <style>
+        body{
+            width: 100%;
+            overflow-x: hidden;
+            padding: 25px;
+        }
+
+        .full-w{
+            max-width: 100%;
+            width: 100%;
+        }
+    </style>
+
+
+
+</head>
+<body>
+
+<img src="{{asset('images/line-01.png')}}" class="full-w"/>
+
+<img src="{{asset('images/line-02.png')}}" class="full-w"/>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
